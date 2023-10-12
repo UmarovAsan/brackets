b27c7369721cd2ae39ef0867dd9042f946330336
asan-31@mail.ru <asan-31@mail.ru>
2023-10-13 02:40:20 +0600

    feat: function for correct bracket sequence

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const stack = [];
+  const openingBrackets = [];
+  const closingBrackets = {};
+  const balancedPairs = new Set();
+
+  for (const pair of bracketsConfig) {
+    const [open, close] = pair;
+    openingBrackets.push(open);
+    closingBrackets[close] = open;
+    if (open === close) {
+      balancedPairs.add(open);
+    }
+  }
+
+  for (const char of str) {
+    if (openingBrackets.includes(char)) {
+      if (!balancedPairs.has(char) || stack[stack.length - 1] !== char) {
+
+        stack.push(char);
+      } else {
+
+        stack.pop();
+      }
+    } else if (closingBrackets[char]) {
+
+      if (stack.length === 0 || stack.pop() !== closingBrackets[char]) {
+        return false;
+      }
+    } else {
+
+    }
+  }
+
+  return stack.length === 0;
+};
